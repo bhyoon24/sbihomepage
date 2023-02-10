dc0b64303b7ef9deb62c98fb693c4440391d096d
YOONBH <bhyoon24@naver.com>
2023-02-11 01:32:28 +0900

    sms 연동

--- sbihome/views.py
+++ sbihome/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import member
 import requests
-import pandas as pd
 import json
-from src.lib import message
 
 # def sbihomepage(request):
 #     return render(request, "index.html", {})
@@ -48,7 +46,7 @@ def sms(request):
             response['error'] = '메세지를 확인하세요.'
         else:
             print(send_msg)
-            member_type = 2
+            member_type = 0     # 수정사항
             base_id = 'appssITu2KHnI0zUO'
             table_id = 'tblLGqfVdDK7C1YH3'
             url = "https://api.airtable.com/v0/" + base_id + "/" + table_id
@@ -61,12 +59,12 @@ def sms(request):
             # 정상 select all
             airtable_response = response.json()
             airtable_records = airtable_response['records']
-            if member_type == 0:
+            if member_type == 2:
                 members = airtable_records
+            elif member_type == 0:  # 유료회원
+                members = [x for x in airtable_records if x['fields']['Status'] == '유료']
             elif member_type == 1:  # 무료회원
                 members = [x for x in airtable_records if x['fields']['Status'] == '무료']
-            elif member_type == 2:  # 무료회원
-                members = [x for x in airtable_records if x['fields']['Status'] == '유료']
 
             phonelist = []
             for p in members:
@@ -75,10 +73,70 @@ def sms(request):
                 phonelist.append(p['fields']['전화번호_010'])
             print(phonelist)
 
+            ########### Solapi ###############
+            #import json
+            import time
+            import datetime
+            import uuid
+            import hmac
+            import hashlib
+            #import requests
+            # apiKey, apiSecret 입력 필수
+            apiKey = 'NCSGSSAS9KEDGZID'
+            apiSecret = 'CWBJSXRAHISCKW65W7UBNX3CIQ5XNETT'
+            # 아래 값은 필요시 수정
+            protocol = 'https'
+            domain = 'api.solapi.com'
+            prefix = ''
 
+            def unique_id():
+                return str(uuid.uuid1().hex)
 
-            response['error'] = '정상 발송되었습니다.'
-        return render(request, 'sendsms.html', response)
+            def get_iso_datetime():
+                utc_offset_sec = time.altzone if time.localtime().tm_isdst else time.timezone
+                utc_offset = datetime.timedelta(seconds=-utc_offset_sec)
+                return datetime.datetime.now().replace(tzinfo=datetime.timezone(offset=utc_offset)).isoformat()
+
+            def get_signature(key, msg):
+                return hmac.new(key.encode(), msg.encode(), hashlib.sha256).hexdigest()
+
+            def get_headers(apiKey, apiSecret):
+                date = get_iso_datetime()
+                salt = unique_id()
+                data = date + salt
+                return {
+                    'Authorization': 'HMAC-SHA256 ApiKey=' + apiKey + ', Date=' + date + ', salt=' + salt + ', signature=' +
+                                     get_signature(apiSecret, data),
+                    'Content-Type': 'application/json; charset=utf-8'
+                }
+
+            def getUrl(path):
+                url = '%s://%s' % (protocol, domain)
+                if prefix != '':
+                    url = url + prefix
+                url = url + path
+                return url
+
+            def sendMany(data):
+                return requests.post(getUrl('/messages/v4/send-many'), headers=get_headers(apiKey, apiSecret),
+                                     json=data)
+
+            # 한번 요청으로 1만건의 메시지 발송이 가능합니다.
+            #if __name__ == '__main__':
+            data = {
+                'messages': [
+                    {
+                        'to': phonelist,
+                        'from': '01089391001',
+                        'text': '[SB인베스트먼트] ' + send_msg + '\n『https://bit.ly/3wBI0BN』'
+                    }
+                ]
+            }
+            res = sendMany(data)
+            print(json.dumps(res.json(), indent=2, ensure_ascii=False))
+
+            # response['error'] = '정상 발송되었습니다.'
+        return render(request, 'sendsms.html')
 
 
 def saveEnquery(request):
